f815fcccffd85779bad34bfd0724dbe5793ce83d
konkubakol <radekraduniek@gmail.com>
2023-01-11 18:09:03 +0100

    v1

---
+++ app/src/main/java/com/example/myapplication/Kraje.java
@@ -0,0 +1,16 @@
+package com.example.myapplication;
+
+public class Kraje {
+
+    String name, capital;
+    int imageId, flagId;
+
+    public Kraje(String name, String capital, int imageId, int flagId) {
+        this.name = name;
+        this.capital = capital;
+        this.imageId = imageId;
+        this.flagId = flagId;
+    }
+
+
+}
