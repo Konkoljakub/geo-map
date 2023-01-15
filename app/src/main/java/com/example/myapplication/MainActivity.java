cdcace13ce8f607044ca5b2a0cdc97c9fdb87ed7
konkubakol <radekraduniek@gmail.com>
2023-01-15 17:30:50 +0100

    v1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //nowy komentarz
         Kraje germany = new Kraje("Niemcy","Berlin", 1, 1);
     }
 
